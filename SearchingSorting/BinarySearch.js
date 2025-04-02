c155f7edf9c61a6d655c0012c74c020a7da1c8ba
Suman Manna <sumanmannacs1996@gmail.com>
2025-04-02 19:00:08 +0530

    Bibart search

---
+++ SearchingSorting/BinarySearch.js
@@ -0,0 +1,86 @@
+// Binary Search
+function binarySearch(list, value) {
+  let start = 0;
+  let end = list.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2);
+    if (list[mid] === value) return true;
+    else if (list[mid] > value) end = mid - 1;
+    else if (list[mid] < value) start = mid + 1;
+  }
+  return false;
+}
+
+console.log(binarySearch([10, 20, 30, 40, 50, 60, 70, 80, 90], 50)); // true
+
+// Get the First Indes Of An Elemnet Of a Sotred List
+function getFirstIndex(list, value) {
+  let start = 0;
+  let end = list.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2);
+    if (list[mid] === value) {
+      if (mid === 0 || list[mid - 1] !== value) return mid;
+      end = mid - 1;
+    } else if (list[mid] > value) {
+      end = mid - 1;
+    } else if (list[mid] < value) {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(getFirstIndex([3, 4, 4, 4, 4, 4, 5, 6], 4)); // 1
+
+// Get the Last Indes Of An Elemnet Of a Sotred List
+function getLastIndex(list, value) {
+  let start = 0;
+  let end = list.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2);
+    if (list[mid] === value) {
+      if (mid === list.length - 1 || list[mid + 1] !== value) return mid;
+      start = mid + 1;
+    } else if (list[mid] > value) {
+      end = mid - 1;
+    } else if (list[mid] < value) {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(getLastIndex([3, 4, 4, 4, 4, 4, 4, 5], 4)); //6
+
+// Find the number of repeated item in a sorted list
+function getNumberOfRepeatedItems(list, value) {
+  const firstIndex = getFirstIndex(list, value);
+  const lastIndex = getLastIndex(list, value);
+  if (firstIndex === -1 || getLastIndex === -1) return 0;
+  else return lastIndex - firstIndex + 1;
+}
+
+console.log(getNumberOfRepeatedItems([3, 4, 4, 4, 4, 4, 4, 5], 4)); // 6
+
+// Find the missing elment on a sorted list each incrimented by 1
+function findMissingElemnet(list) {
+  let start = 0;
+  let end = list.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2);
+    if (list[mid] !== list[0] + mid) {
+      if (list[mid - 1] == list[0] + mid - 1) return list[mid] - 1;
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return list[list.length - 1] + 1;
+}
+
+console.log(findMissingElemnet([10, 11, 12, 13, 15, 16, 17, 18, 19])); // 14
---
+++ SearchingSorting/BinarySearchRecursion.js
@@ -0,0 +1,68 @@
+// Binary search
+function binarySearchRecursion(list, value, start = 0, end = list.length - 1) {
+  if (start > end) return false;
+  const mid = Math.floor(start + (end - start) / 2);
+  if (list[mid] === value) return true;
+  else if (list[mid] > value)
+    return binarySearchRecursion(list, value, start, mid - 1);
+  else if (list[mid] < value)
+    return binarySearchRecursion(list, value, mid + 1, end);
+}
+
+console.log(binarySearchRecursion([10, 20, 30, 40, 50, 60, 70, 80, 90], 20));
+
+// Get the First Indes Of An Elemnet Of a Sotred List
+function getFirstIndexRecursion(list, value, start = 0, end = list.length - 1) {
+  if (start > end) return -1;
+  const mid = Math.floor(start + (end - start) / 2);
+  if (list[mid] === value) {
+    if (mid === 0 || list[mid - 1] !== value) return mid;
+    return getFirstIndexRecursion(list, value, start, mid - 1);
+  } else if (list[mid] > value) {
+    return getFirstIndexRecursion(list, value, start, mid - 1);
+  } else if (list[mid] < value) {
+    return getFirstIndexRecursion(list, value, mid + 1, end);
+  }
+}
+
+console.log(getFirstIndexRecursion([3, 4, 4, 4, 4, 4, 5, 6], 4));
+
+// Get the Last Indes Of An Elemnet Of a Sotred List
+function getLastIndexRecursion(list, value, start = 0, end = list.length - 1) {
+  if (start > end) return -1;
+  const mid = Math.floor(start + (end - start) / 2);
+  if (list[mid] === value) {
+    if (mid === list.length - 1 || list[mid + 1] !== value) return mid;
+    return getLastIndexRecursion(list, value, mid + 1, end);
+  } else if (list[mid] > value) {
+    return getLastIndexRecursion(list, value, start, mid - 1);
+  } else if (list[mid] < value) {
+    return getLastIndexRecursion(list, value, mid + 1, end);
+  }
+}
+
+console.log(getLastIndexRecursion([3, 4, 4, 4, 4, 4, 4, 5], 4)); // 6
+
+// Find the number of repeated item in a sorted list
+function getNumberOfRepeatedItemsRecursion(list, value) {
+  const firstIndex = getFirstIndexRecursion(list, value);
+  const lastIndex = getLastIndexRecursion(list, value);
+  if (firstIndex === -1 || lastIndex === -1) return 0;
+  else return lastIndex - firstIndex + 1;
+}
+
+console.log(getNumberOfRepeatedItemsRecursion([3, 4, 4, 4, 4, 4, 4, 5], 4)); // 6
+
+// Find the missing elment on a sorted list each incrimented by 1
+function findMissingElemnetRecursion(list, start = 0, end = list.length - 1) {
+  if (start > end) return list[list.length - 1] + 1;
+  const mid = Math.floor(start + (end - start) / 2);
+  if (list[mid] !== list[0] + mid) {
+    if (list[mid - 1] === list[0] + mid - 1) return list[mid] - 1;
+    return findMissingElemnetRecursion(list, start, mid - 1);
+  } else {
+    return findMissingElemnetRecursion(list, mid + 1, end);
+  }
+}
+
+console.log(findMissingElemnetRecursion([10, 11, 12, 13, 15, 16, 17, 18, 19])); // 14
