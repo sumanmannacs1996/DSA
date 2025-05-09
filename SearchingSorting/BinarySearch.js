0823b43325a7ba43969f62cc6d000014c9897f33
sumanmannacs1996 <78676771+sumanmannacs1996@users.noreply.github.com>
2025-05-09 18:13:09 +0530

    Update BinarySearch.js

--- SearchingSorting/BinarySearch.js
+++ SearchingSorting/BinarySearch.js
@@ -106,3 +106,16 @@ function peakIndexInMountainArray(list) {
 }
 
 console.log(peakIndexInMountainArray([10, 20, 30, 90, 70, 60, 50, 40]));
+// alternative
+function getPickIndexOfAMountainArray(list) {
+  let start = 0,
+    end = list.length - 1;
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2);
+    if (list[mid] > list[mid - 1] && list[mid] > list[mid + 1]) return mid;
+    else if (list[mid] > list[mid - 1]) start = mid + 1;
+    else if (list[mid] > list[mid + 1]) end = mid - 1;
+  }
+}
+
+console.log(getPickIndexOfAMountainArray([10, 20, 30, 40, 50, 60, 70, 80, 90, 70, 60, 50, 40]));
